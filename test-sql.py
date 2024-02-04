e0600f035e25a12ff87a218c8a71fce4aed24453
hookabidoo <rubyhongle.116@gmail.com>
2024-02-04 17:40:19 +1100

    updated

--- test-sql.py
+++ test-sql.py
@@ -1,6 +1,6 @@
 # check the GA to see if it pick up anything!
 def search():
-    My_variable = "password:thsdbjwbdjwbk46757"
+    My_variable = "password:SuprSecret1234!"
     code = request.args.get('code')
     conn = sqlite3.connect("data.db")
     c = conn.cursor()
