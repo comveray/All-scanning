4e4ab14b43bf58cc4c710343b15fc7bc301c1ab1
Hookabidoo <44333642+hookabidoo@users.noreply.github.com>
2024-02-04 16:09:14 +1100

    Add files via upload

--- test-sql.py
+++ test-sql.py
@@ -1,3 +1,4 @@
+# this is a test for pr GA
 def search():
     code = request.args.get('code')
     conn = sqlite3.connect("data.db")
@@ -11,5 +12,4 @@ def search():
         else:
             return f"Wifi Connection Established<br>{statement}"
     except sqlite3.Error as e:
-        return str(e) + f"<br>{statement}
-
+        return str(e) + "<br>{statement}
