8e9cea38d2a15bdcdd24411077f6807403bf6d90
hookabidoo <rubyhongle.116@gmail.com>
2024-02-06 14:23:14 +1100

    Update PR_Secret_scanning.yml

---
+++ test-sql.py
@@ -0,0 +1,19 @@
+# check the GA to see if it pick up anything! Hopefully 
+def search():
+    code = request.args.get('code')
+    conn = sqlite3.connect("data.db")
+    c = conn.cursor()
+    try:
+        statement = "select * from data where data='" + code + "'"
+        c.execute(statement)
+        found = c.fetchall()
+        if found == []:
+            return f"Invalid Code<br>{statement}"
+        else:
+            return f"Wifi Connection Established<br>{statement}"
+    except sqlite3.Error as e:
+        return str(e) + "<br>{statement}
+
+password1 = 'VnDduv#7862-9s8hdv%2hMuh'
+aws_akid1 = 'AKIAIOSFODNN7N0EFXMP'
+aws_sk1 = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYBNA2dBkDTiv'
